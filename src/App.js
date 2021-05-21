4f8d7e0c5079262eab496a9428c0cc77990fcfda
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-05-21 19:17:33 +0600

    data passed through mapping

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import News from './components/News'
+import { useEffect, useState } from 'react';
 function App() {
+  const [articles,setArticles] =  useState([]);
+  useEffect(() =>{
+    const url = "https://newsapi.org/v2/everything?q=tesla&from=2021-04-21&sortBy=publishedAt&apiKey=ccf5ab0c03404bd6bf16d40f5126e13a"
+    fetch(url)
+    .then(response => response.json())
+    .then(data=>setArticles(data.articles))
+  },[])
   return (
     <div className="App">
-      
-      <News></News>
+    <h2>get latest news</h2>
+    {
+      articles.map(article=> <News key={article.id} article={article}></News>)
+    }      
     </div>
   );
 }
--- src/components/News.js
+++ src/components/News.js
@@ -1,17 +1,56 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const news = () => {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+
+
+const News = (props) => {
+    const classes = useStyles();
+    const {title,description,urlToImage}=props.article
+
     return (
-        <div>
-            <h2>this is news</h2>
-            <Button variant="contained">Default</Button>
-            <Button variant="contained" color="secondary">
-  Secondary
-</Button>
-
-        </div>
+      <> 
+    <Card className={classes.root}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        alt="Contemplative Reptile"
+        height="140"
+        image={urlToImage}
+        title="hkldjk"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+        {title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+        {description}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+        Share
+      </Button>
+      <Button size="small" color="primary">
+        Learn More
+      </Button>
+    </CardActions>
+  </Card>  
+  </>
     );
 };
 
-export default news;
+export default News;
